533685a40a9a1195b3b1ac4e615f973c9baa60bc
Richard Osborne <richard@xmos.com>
2012-05-09 22:36:51 +0100

    Libraries in `llvm-config --ldflags` must be passed after other libraries.

--- cmake/Modules/FindLLVM.cmake
+++ cmake/Modules/FindLLVM.cmake
@@ -4,6 +4,7 @@
 #  LLVM_CONFIG_EXECUTABLE - The llvm-config executable
 #  LLVM_CFLAGS - C preprocessor flags for files that include LLVM headers
 #  LLVM_LDFLAGS - Linker flags needed to link against LLVM
+#  LLVM_LIBRARIES - Libraries needed to link against LLVM
 
 find_program(LLVM_CONFIG_EXECUTABLE
   NAMES llvm-config
@@ -24,16 +25,28 @@ if (LLVM_FOUND)
   )
 
   execute_process(
-    COMMAND ${LLVM_CONFIG_EXECUTABLE} --ldflags
-    OUTPUT_VARIABLE LLVM_LDFLAGS
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
-
-  execute_process(
     COMMAND ${LLVM_CONFIG_EXECUTABLE} --libs all
     OUTPUT_VARIABLE LLVM_LIBRARIES
     OUTPUT_STRIP_TRAILING_WHITESPACE
   )
+
+  execute_process(
+    COMMAND ${LLVM_CONFIG_EXECUTABLE} --ldflags
+    OUTPUT_VARIABLE LLVM_LDFLAGS_RAW
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+
+  # Add any libraries that appear in llvm-config --ldflags to LLVM_LIBRARIES,
+  # add other flags to LLVM_LDFLAGS.
+  string(REPLACE " " ";" LLVM_LDFLAGS_LIST ${LLVM_LDFLAGS_RAW})
+  foreach(flag ${LLVM_LDFLAGS_LIST})
+    if(${flag} MATCHES "^-l")
+      SET(LLVM_LIBRARIES "${LLVM_LIBRARIES} ${flag}")
+    else()
+      SET(LLVM_LDFLAGS "${LLVM_LDFLAGS} ${flag}")
+    endif()
+  endforeach()
+
 endif()
 
 mark_as_advanced(LLVM_CONFIG_EXECUTABLE)
