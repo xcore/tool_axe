6d9a18f14847f3cf051b1dec6f1025b3c64ce3c5
Richard Osborne <richard@xmos.com>
2012-10-23 09:02:09 +0100

    Add custom FindSDL which uses sdl-config to find SDL.
    This ensures the correct libraries are linked when static libraries
    are used.

---
+++ cmake/Modules/FindSDL.cmake
@@ -0,0 +1,32 @@
+# Locate SDL
+# This module defines
+#  SDL_FOUND - System has SDL
+#  SDL_CONFIG_EXECUTABLE - The sdl-config executable
+#  SDL_CFLAGS - C preprocessor flags for files that include SDL headers
+#  SDL_LIBRARIES - Libraries needed to link against SDL
+
+find_program(SDL_CONFIG_EXECUTABLE
+  NAMES sdl-config
+  DOC "sdl-config executable")
+
+include(FindPackageHandleStandardArgs)
+# Sets SDL_FOUND
+find_package_handle_standard_args(
+  SDL DEFAULT_MSG SDL_CONFIG_EXECUTABLE)
+
+if (SDL_FOUND)
+  execute_process(
+    COMMAND ${SDL_CONFIG_EXECUTABLE} --cflags
+    OUTPUT_VARIABLE SDL_CFLAGS
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+
+  execute_process(
+    COMMAND ${SDL_CONFIG_EXECUTABLE} --libs
+    OUTPUT_VARIABLE SDL_LIBRARIES
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+
+endif()
+
+mark_as_advanced(SDL_CONFIG_EXECUTABLE)
