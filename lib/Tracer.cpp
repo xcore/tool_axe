1ac31a5fe23862ab045e8136f5a7e73e2e7925f1
Richard Osborne <richard@xmos.com>
2013-06-08 20:43:50 +0100

    Provide default implementations of the Tracer::methods which do nothing.

--- lib/Tracer.cpp
+++ lib/Tracer.cpp
@@ -10,3 +10,69 @@ using namespace axe;
 Tracer::~Tracer()
 {
 }
+
+void Tracer::instructionBegin(const Thread &t)
+{
+}
+
+void Tracer::regWrite(Register::Reg reg, uint32_t value)
+{
+}
+
+void Tracer::instructionEnd()
+{
+}
+
+void Tracer::SSwitchRead(const Node &node, uint32_t retAddress,
+                         uint16_t regNum)
+{
+}
+
+void Tracer::SSwitchWrite(const Node &node, uint32_t retAddress,
+                          uint16_t regNum, uint32_t value)
+{
+}
+
+void Tracer::SSwitchNack(const Node &node, uint32_t dest)
+{
+}
+
+void Tracer::SSwitchAck(const Node &node, uint32_t dest)
+{
+}
+
+void Tracer::SSwitchAck(const Node &node, uint32_t data, uint32_t dest)
+{
+}
+
+void Tracer::exception(const Thread &t, uint32_t et, uint32_t ed,
+                       uint32_t sed, uint32_t ssr, uint32_t spc)
+{
+}
+
+void Tracer::event(const Thread &t, const EventableResource &res, uint32_t pc,
+                   uint32_t ev)
+{
+}
+
+void Tracer::interrupt(const Thread &t, const EventableResource &res,
+                       uint32_t pc, uint32_t ssr, uint32_t spc, uint32_t sed,
+                       uint32_t ed)
+{
+}
+
+void Tracer::syscall(const Thread &t, const std::string &s)
+{
+}
+
+void Tracer::syscall(const Thread &t, const std::string &s, uint32_t op0)
+{
+}
+
+void Tracer::timeout(const SystemState &system, ticks_t time)
+{
+}
+
+void Tracer::noRunnableThreads(const SystemState &system)
+{
+}
--- lib/Tracer.h
+++ lib/Tracer.h
@@ -34,35 +34,35 @@ class Tracer {
 public:
   virtual ~Tracer();
 
-  virtual void instructionBegin(const Thread &t) = 0;
+  virtual void instructionBegin(const Thread &t);
 
-  virtual void regWrite(Register::Reg reg, uint32_t value) = 0;
+  virtual void regWrite(Register::Reg reg, uint32_t value);
 
-  virtual void instructionEnd() = 0;
+  virtual void instructionEnd();
 
   virtual void SSwitchRead(const Node &node, uint32_t retAddress,
-                           uint16_t regNum) = 0;
+                           uint16_t regNum);
   virtual void SSwitchWrite(const Node &node, uint32_t retAddress,
-                            uint16_t regNum, uint32_t value) = 0;
-  virtual void SSwitchNack(const Node &node, uint32_t dest) = 0;
-  virtual void SSwitchAck(const Node &node, uint32_t dest) = 0;
-  virtual void SSwitchAck(const Node &node, uint32_t data, uint32_t dest) = 0;
+                            uint16_t regNum, uint32_t value);
+  virtual void SSwitchNack(const Node &node, uint32_t dest);
+  virtual void SSwitchAck(const Node &node, uint32_t dest);
+  virtual void SSwitchAck(const Node &node, uint32_t data, uint32_t dest);
 
   virtual void exception(const Thread &t, uint32_t et, uint32_t ed,
-                         uint32_t sed, uint32_t ssr, uint32_t spc) = 0;
+                         uint32_t sed, uint32_t ssr, uint32_t spc);
 
   virtual void event(const Thread &t, const EventableResource &res, uint32_t pc,
-                     uint32_t ev) = 0;
+                     uint32_t ev);
 
   virtual void interrupt(const Thread &t, const EventableResource &res,
                          uint32_t pc, uint32_t ssr, uint32_t spc, uint32_t sed,
-                         uint32_t ed) = 0;
+                         uint32_t ed);
 
-  virtual void syscall(const Thread &t, const std::string &s) = 0;
+  virtual void syscall(const Thread &t, const std::string &s);
   // TODO provide a more sensible interface.
-  virtual void syscall(const Thread &t, const std::string &s, uint32_t op0) = 0;
-  virtual void timeout(const SystemState &system, ticks_t time) = 0;
-  virtual void noRunnableThreads(const SystemState &system) = 0;
+  virtual void syscall(const Thread &t, const std::string &s, uint32_t op0);
+  virtual void timeout(const SystemState &system, ticks_t time);
+  virtual void noRunnableThreads(const SystemState &system);
 };
 
 } // End axe namespace
