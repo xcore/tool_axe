1b7b6c284f63019ec9dbc86eed38150c54688840
Richard Osborne <richard@xmos.com>
2014-02-19 16:43:28 +0000

    Update FindLLVM to work in the absence of llvm-config.
    This is needed to link against a versions of LLVM built using Ninja on
    Windows.

--- cmake/Modules/FindLLVM.cmake
+++ cmake/Modules/FindLLVM.cmake
@@ -6,6 +6,11 @@
 #  LLVM_LDFLAGS - Linker flags needed to link against LLVM
 #  LLVM_LIBRARIES - Libraries needed to link against LLVM
 
+# LLVM installs a LLVMConfig.cmake file if it was built using cmake. It also
+# installs an executable (llvm-config) if it wasn't built on Windows.
+# Since there is no method of finding LLVM which works in all situations we
+# try both in turn, i.e. we first look for a llvm-config variable and if that
+# fails we try and find the cmake config.
 find_program(LLVM_CONFIG_EXECUTABLE
   NAMES llvm-config-${LLVM_FIND_VERSION} llvm-config
   DOC "llvm-config executable")
@@ -55,10 +60,30 @@ if (LLVM_CONFIG_EXECUTABLE)
     list(APPEND LLVM_LIBRARIES ${LLVM_LIBRARY})
   endforeach()
 
+  find_package_handle_standard_args(
+    LLVM REQUIRED_VARS LLVM_CONFIG_EXECUTABLE
+         VERSION_VAR LLVM_VERSION)
+else()
+  # LLVMConfigVersion.cmake nevers sets PACKAGE_VERSION_COMPATIBLE which means
+  # that searching for a specific version doesn't work. Workaround this by
+  # temporarily unsetting LLVM_FIND_VERSION.
+  set(LLVM_FIND_VERSION_SAVED ${LLVM_FIND_VERSION})
+  set(LLVM_FIND_VERSION)
+  find_package(LLVM NO_MODULE REQUIRED)
+  set(LLVM_FIND_VERSION ${LLVM_FIND_VERSION_SAVED})
+  if (LLVM_FOUND)
+    message(LLVM_FOUND)
+    llvm_map_components_to_libraries(LLVM_LIBNAMES scalaropts engine bitreader)
+
+    foreach(LLVM_LIBNAME ${LLVM_LIBNAMES})
+      # Don't cache the library location as we are searching for a different library
+      # each time around the loop.
+      set(LLVM_LIBRARY ${LLVM_LIBNAME}-NOTFOUND)
+      find_library(LLVM_LIBRARY ${LLVM_LIBNAME}
+                   PATHS ${LLVM_LIBRARY_DIRS})
+      list(APPEND LLVM_LIBRARIES ${LLVM_LIBRARY})
+    endforeach()
+  endif()
+  find_package_handle_standard_args(LLVM CONFIG_MODE)
 endif()
-
-find_package_handle_standard_args(
-  LLVM REQUIRED_VARS LLVM_CONFIG_EXECUTABLE
-       VERSION_VAR LLVM_VERSION)
-
 mark_as_advanced(LLVM_CONFIG_EXECUTABLE)
