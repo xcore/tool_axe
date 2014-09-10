148bef8619c04647e6d310b8aa315fff8f1bab38
Simon Cooksey <sjc80@kent.ac.uk>
2014-09-10 16:05:35 +0100

    Removed queue skipping for single step thread. Owness will now be on caller to clear the run queue before calling this function

--- lib/axe.cpp
+++ lib/axe.cpp
@@ -161,9 +161,7 @@ void axeStepThreadOnce(AXEThreadRef thread)
 {
   SystemState *sys = unwrap(thread)->getParent().getParent()->getParent();
   Thread *t = unwrap(thread);
-  // To prevent this thread yeilding to others while single stepping, we need to
-  //  set it's last run time to the oldest in the Runable Queue.
-  t->setTime(sys->getEarliestThreadTime());
+
   t->singleStep();
 }
 
