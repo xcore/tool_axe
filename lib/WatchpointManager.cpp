0810ad5b8f5dac26f4269cbf3919ac9c404d6ec8
Simon Cooksey <sjc80@kent.ac.uk>
2014-09-15 14:18:55 +0100

    Implemented unset watchpoint

--- lib/WatchpointManager.cpp
+++ lib/WatchpointManager.cpp
@@ -13,6 +13,15 @@ void WatchpointManager::setWatchpoint(WatchpointType type, uint32_t lowAddr, uin
 void WatchpointManager::unsetWatchpoint(WatchpointType type, uint32_t lowAddr, uint32_t highAddr)
 {
 	Watchpoint w = Watchpoint(type, std::pair<uint32_t, uint32_t>(lowAddr, highAddr));
+	watchpointsIterator = watchpoints.begin();
+	while(watchpointsIterator != watchpoints.end())
+	{
+		if((*watchpointsIterator).first == type &&				// Check same type
+		 	(*watchpointsIterator).second.first == lowAddr &&	// Check addr == lower bound
+		  	(*watchpointsIterator).second.second == highAddr)	// Check addr == upper bound
+			watchpoints.erase(watchpointsIterator);
+		std::advance(watchpointsIterator, 1);
+	}
 }
 
 bool WatchpointManager::contains(Watchpoint w)
