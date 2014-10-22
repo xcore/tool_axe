7ce41198a1e6b21448932e3644889e3afd16f23b
Simon Cooksey <sjc80@kent.ac.uk>
2014-10-22 11:32:22 +0100

    Added test for finding LLVMSymbolSearching bug This seems to compile to include a reference to `__udivi3` in JIT'd code. This gets breaks when symbol seaching is disabled on 32 bit platforms with a segfault in JIT'd code. This test should find exhibit that.

---
+++ test/fibonacci.c
@@ -0,0 +1,37 @@
+/*
+ * RUN: xcc -target=XK-1A %s -o %t1.xe
+ * RUN: axe %t1.xe
+ */
+
+///////////////////////////////////////////////////////////
+//recursion
+///////////////////////////////////////////////////////////
+
+#include "stdlib.h"
+#include "stdio.h"
+
+///////////////////////////////////////////////////////////
+//Main
+///////////////////////////////////////////////////////////
+
+int main(void) 
+{
+    int fibonacci_depth = 80;  		// The number of fibonacci numbers we will print 
+    int i = 0;        				// The index of fibonacci number to be printed next
+    unsigned long long current = 1; // The value of the (i)th fibonacci number
+    unsigned long long next = 1;    // The value of the (i+1)th fibonacci number
+    unsigned long long twoaway = 0; // The value of the (i+2)th fibonacci number
+
+	printf("\tI \t Fibonacci(I) \n\t=====================\n");
+ 
+	for (i=1; i<=fibonacci_depth; i++) 
+	{
+		printf("\t%d \t   %llu\n", i, current);
+		twoaway = current+next;
+		current = next;
+		next    = twoaway;
+    }
+
+	return EXIT_SUCCESS;
+}
+
